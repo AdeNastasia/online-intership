921a52fcbc7ef92b6558227f34b2d734a22b448f
AdeNastasia <ade.nastasia@gmail.com>
2022-09-06 09:44:45 +0300

    add class player

---
+++ src/main/java/com/game/entity/Player.java
@@ -0,0 +1,16 @@
+package com.game.entity;
+
+import java.util.Date;
+
+public class Player {
+    Long id;
+    String name;
+    String title;
+    Race race;
+    Profession profession;
+    Integer experience;
+    Integer level;
+    Integer untilNextLevel;
+    Date Birthday;
+    Boolean banned;
+}
